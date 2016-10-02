e781ac653b6fb5083acc892203eca2a18ef448a0
TheilCamille <camille@theil.fr>
2016-10-02 19:38:51 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('#b1').click(function(){
+
+		$('.art1').toggle('1');
+
+		});
+	$("#b2").click(function(){
+
+		$('.art2').toggle('1');
+
+		});
+	$('#b3').click(function(){
+
+		$('.art3').toggle('1');
+
+		});
+});
